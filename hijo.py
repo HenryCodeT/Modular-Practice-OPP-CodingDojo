e074de60556e0c63a57b3ab14e871bbc88bc26ae
HenryCodeT <120686@unsaac.edu.pe>
2022-01-19 10:12:49 -0500

    first commit

---
+++ hijo.py
@@ -0,0 +1,7 @@
+import padre    
+
+# en el mismo archivo, agrega lo siguiente debajo de la clase Usuario
+# print(padre.square(5))
+# user = padre.Usuario("Anna")
+# print(user.name)
+# print(user.di_hola())
