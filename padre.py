b09dfb8ddcc4d238e33b7c06afce0b054d42d15a
HenryCodeT <120686@unsaac.edu.pe>
2022-01-19 11:28:52 -0500

    add part 10

--- padre.py
+++ padre.py
@@ -1,13 +1,15 @@
+from store import Producto
 local_val = "unicornios mágicos"
 def square(x):
     return x * x
-class Usuario:
+
+class Usuario(Producto):
     def __init__(self, name):
         self.name = name
     def di_hola(self):
         return "hola"
 if __name__ == "__main__":
-    producto = Producto([args])
+    producto = Producto()
     print(producto)
     print(producto.agregar_impuesto(0.18))
 else:
